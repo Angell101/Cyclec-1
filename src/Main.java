aa6c651d67f1c0298dc91acd57c37d5cf06e168b
Angell <Sa1Lo_xd@vk.cpm>
2023-11-08 21:00:42 +1100

    Cyclec-1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1();// задача 1
+        //task2();// задача 2
+        //task3();// задача 3
+        //task4();// задача 4
+        //task5();// задача 5
+        //task6();// задача 6
+        //task7();// задача 7
+        //task8();// задача 8
+        //task9();// задача 9
+        //task10();// задача 10
+    }
+}
